1878bcb5b5e4d9e4eb5171cb220cfc868a444d15
Arif Ebrahim <arifebrahim@Arifs-MacBook-Pro.local>
2021-08-12 03:57:10 +0100

    refactored using hooks

--- src/TutorList/TutorList.js
+++ src/TutorList/TutorList.js
@@ -1,39 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Tutor from './Tutor'
 
 const api_url = 'https://tutoroom.herokuapp.com/api/users'
 
-class TutorList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tutors: [],
-    }
-  }
+export default function TutorList() {
+  const [tutors, setTutors] = useState([])
 
-  componentDidMount() {
-    this.getTutors();
-  }
+  useEffect(() => {
+    getTutors();
+  })
 
-  getTutors() {
+  const getTutors = () => {
     fetch(api_url)
     .then(response => response.json())
     .then(tutors => {
-      this.setState({
-        tutors: tutors,
-      })
+      setTutors(tutors)
     })
   }
 
-  render() {
-    return (
-      <div className='tutor-list'>
-        {this.state.tutors.map((tutor) => (
-          <Tutor key={tutor.id} tutor={tutor}/>
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div className='tutor-list'>
+    {tutors.map((tutor) => (
+      <Tutor key={tutor.id} tutor={tutor}/>
+    ))}
+    </div>
+  )
 }
-
-export default TutorList
