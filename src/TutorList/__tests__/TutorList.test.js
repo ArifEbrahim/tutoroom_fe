20a5c74de0d4724aaa118b834ab930979e5b740a
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-13 13:51:20 +0100

    clean up tests

--- src/TutorList/__tests__/TutorList.test.js
+++ src/TutorList/__tests__/TutorList.test.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react'
 import TutorList from '../TutorList'
-import axios from '../__mocks__/axios'
+import axios from 'axios'
 
-// jest.mock('axios')
-// global.axios = axios
+jest.mock('axios')
 test('it makes a call to the heroku api',  async () => {
-  // let container = document.createElement('div');
-  // jest.spyOn(axios, 'get').mockResolvedValue({ data: [] });
   axios.get.mockResolvedValueOnce({ data: []})
-  // return act(() => {
-  //   return Promise.resolve({ data: []})
-  // })
-  // act(() => {
-    render(<TutorList />)
-  // })
+  render(<TutorList />)
   await waitFor(() => {
     expect(axios.get).toHaveBeenCalled()
-  });
-  
+  })
 })
