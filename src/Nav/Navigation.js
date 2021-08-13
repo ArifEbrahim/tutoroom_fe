83bf26aa1e417d6259b6f13791f1ac9684361f40
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-13 19:14:57 +0100

    extracted navbar to component

---
+++ src/Nav/Navigation.js
@@ -0,0 +1,21 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import React from 'react'
+
+export default function Navigation() {
+  return (
+<Navbar bg="primary" variant="dark" expand="md">
+  <Container>
+    <Navbar.Brand as='h1'>Tutoroom</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+      <Nav.Link as={Link} to="/search">Search</Nav.Link>
+      <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+      <Nav.Link as={Link} to="/signout">Sign Out</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+  )
+}
