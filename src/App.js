e099c6a8187bdae259519d8e0a704d499b5ebeda
OptimusWall-E <iemailedmichael@gmail.com>
2021-08-13 15:03:11 +0100

    Added new routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import './App.css';
-import TutorList from './TutorList/TutorList';
+import Home from './auth/Login';
+import Signup from './auth/Signup';
+import TutorList from './tutorList/TutorList';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <TutorList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/search">
+            <TutorList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+// import './App.css';
+// import TutorList from './TutorList/TutorList';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <TutorList />
+//     </div>
+//   );
+// }
+
+// export default App;
