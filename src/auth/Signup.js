a3d712f72cdffcd11400e2a1fe7b695a7ce85b6e
OptimusWall-E <iemailedmichael@gmail.com>
2021-08-13 15:54:08 +0100

    new branch for signup page

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,9 +1,79 @@
 import React from 'react'
+import { useState } from 'react'
+// import axios from 'axios'
 
 export default function Signup() {
+  const [userType, setUserType] = useState("student");
+  const [subject, setSubject] = useState(" ");
+  const [email, setEmail] = useState("email");
+  const [password, setPassword] = useState("password");
+  const [confirmedPassword, setConfirmedPassword] = useState("confirm password");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {userType, subject, email, password, confirmedPassword};
+    console.log(user)
+
+    fetch('https://tutoroom.herokuapp.com/api/users', {
+      method: 'POST', 
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(user)
+    }).then(() => {
+      console.log(user)
+    })
+  }
+  
   return (
-    <div>
-      <h2>Signup</h2>
+    <div className="signup-form">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input 
+            type="radio"
+            value={userType}
+            onChange={(e) => setUserType(e.target.value)}
+            />
+            Student
+        </label>
+        
+        <label>
+          <input 
+            type="radio"
+            value={userType}
+            onChange={(e) => setUserType(e.target.value)}
+            />
+            Tutor
+        </label>
+
+        <label>Subject</label>
+          <input 
+            type="text"
+            required
+            placeholder=" "
+            onChange={(e) => setSubject(e.target.value)}
+          /><br/>
+        <label>Email address</label>
+        <input 
+          type="text"
+          required
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+        <label>New password</label>
+        <input 
+          type="text"
+          required
+          placeholder="choose password"
+          onChange={(e) => setPassword(e.target.value)}
+        /><br/>
+        <label>Confirm password</label>
+        <input 
+          type="text"
+          required
+          placeholder="confirm password"
+          onChange={(e) => setConfirmedPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
