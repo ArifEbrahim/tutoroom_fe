34d29c5b72dc697cb81e795181c1db6ab7711bb5
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-13 23:15:06 +0100

    centered text on search cards

--- src/TutorList/Tutor.js
+++ src/TutorList/Tutor.js
@@ -7,7 +7,7 @@ export default function Tutor(props) {
     <div className='tutor'>
       <Card style={{ width: '16rem' }}>
         <Card.Img variant="top" src='https://upload.wikimedia.org/wikipedia/commons/a/a9/Prime_Minister_David_Cameron_-_official_photograph_%288947770804%29_%28cropped%29.jpg'/>
-        <Card.Body>
+        <Card.Body className='text-center'>
           <Card.Title>{props.tutor.name}</Card.Title>
           <Card.Text>{props.tutor.bio}</Card.Text>
           <Button as={Link} to={`/search/${props.tutor.id}`}>View Profile</Button>
