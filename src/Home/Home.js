79870a52f6ef8fa954820dc77157a5f6110378b5
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-15 12:38:48 +0100

    login working

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 export default function Home() {
     return (
         <div>
-            
+            <h1>Home page</h1>
         </div>
     )
 }
--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 export default function Login() {
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+  const api_url='http://localhost:3001/api/users/login'
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    axios.post(api_url, {
+      "email": email,
+      "password": password
+    })
+    .then(response => {
+      console.log(response)
+      history.push('/search')
+    })
+
+  }
+
   return (
     <div>
-      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Email address: </label>
+        <input 
+          type='email'
+          placeholder={email}
+          onChange={(e)=> setEmail(e.target.value)}
+        />
+        <label>Password: </label>
+        <input 
+          type='password'
+          placeholder={password}
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
