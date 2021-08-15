90d23f1a99e03502910de4807f3818f6f5c003c8
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-15 13:06:23 +0100

    removed spare submit call

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -30,13 +30,13 @@ export default function Login() {
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <Form.Control type="email" required placeholder={email} onChange={(e)=> setEmail(e.target.value)}/>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder={password} onChange={(e)=> setPassword(e.target.value)}/>
+                <Form.Control type='password' required placeholder={password} onChange={(e)=> setPassword(e.target.value)}/>
               </Form.Group>
-              <Button variant="primary" type="submit" onClick={handleSubmit}>
+              <Button variant="primary" type="submit">
               Submit
               </Button>
             </Form>
